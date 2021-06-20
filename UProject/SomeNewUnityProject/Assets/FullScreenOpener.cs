c21adb7c3aaa9c7d578d086f71fa19be7c81ac8a
Nabr1n <nabr1nart@gmail.com>
2021-06-21 01:20:58 +0300

    водарасли

---
+++ UProject/SomeNewUnityProject/Assets/FullScreenOpener.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullScreenOpener : MonoBehaviour
+{
+    [SerializeField] private bool ShouldOpenfullScreen;
+    [SerializeField] private Vector2Int Resolution;
+    void Start()
+    {
+        if(ShouldOpenfullScreen){
+            Screen.SetResolution(Resolution.x, Resolution.y, true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
